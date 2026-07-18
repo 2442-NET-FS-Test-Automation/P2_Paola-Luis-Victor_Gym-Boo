09199de24a701f95a9ea2f3887c003afef971515
Victor <victoriyari@hotmail.com>
2026-07-18 11:52:42 -0600

    Some folders, still waiting for the final Db diagram

---
+++ Gym_Boo.Data/Enum/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace Gym_Boo.Data.Enum;
+
+public enum UserRole
+{
+    admin,
+    instructor,
+    user
+}
---
+++ Gym_Boo.Data/GymBooDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gym_Boo.Data;
+
+public class GymBooDbContext : DbContext
+{
+    public GymBooDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    
+}
