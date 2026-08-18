fa13c830794305931ed35d58d5e85846bbe629af
Victor <naragcuruga@gmail.com>
2026-08-18 16:34:31 -0600

    e

--- BackEnd/Gym_Boo.Controllers/Services/Interfaces/IInstructorServices.cs
+++ BackEnd/Gym_Boo.Controllers/Services/Interfaces/IInstructorServices.cs
@@ -7,11 +7,11 @@ namespace Gym_Boo.Controllers.Services.Interfaces;
 public interface IInstructorServices
 {
     Task<User?> GetInstructor(int id, CancellationToken ct);
-    Task<bool> NewSession(Session session, CancellationToken ct);
+    Task NewSession(Session session, CancellationToken ct);
     Task<SessionAttendanceResponseDto> GetAttendance(int id, CancellationToken ct);
     Task<List<UpcomingSessionDto>> GetUpcomingSessionsForInstructor(int instructorId, CancellationToken ct);
     Task<List<ClassOptionDto>> GetClassOptions(CancellationToken ct);
     Task<List<PlaceOptionDto>> GetPlaceOptions(CancellationToken ct);
-    Task<bool> DeleteSession(int id, CancellationToken ct);
-    Task<bool> TakeAttendance(TakingAttendanceDTO dto, CancellationToken ct = default);
+    Task DeleteSession(int id, CancellationToken ct);
+    Task TakeAttendance(TakingAttendanceDTO dto, CancellationToken ct = default);
 }
